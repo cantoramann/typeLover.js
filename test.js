d8cee0117bcbeb9063620406ee19b3fc3b580c3f
Can Toraman <toraman@usc.edu>
2020-10-20 16:50:44 +0300

    Initial Commit

---
+++ test.js
@@ -0,0 +1,24 @@
+let animate = document.querySelectorAll('.type-lover');
+let dictionary = [];
+for (var i = 0; i < animate.length; i++) {
+    dictionary.push({
+        text: animate[i].innerHTML,
+        delayTime: parseFloat(animate[i].getAttribute("animation-delay")),
+        object: animate[i]
+    });
+    animate[i].innerHTML = "";
+}
+
+for (var i = 0; i < animate.length; i++) {
+
+    for (var j = 0; j < dictionary[i].text.length; j++) {
+        delayAnim(i, j, dictionary[i].text);
+    }
+}
+
+function delayAnim(i, j, str) {
+    setTimeout(function () {
+        dictionary[i].object.innerHTML += dictionary[i].text.charAt(j);
+    }, 50 * j);
+}
+
